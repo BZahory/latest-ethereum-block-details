df90cba1878d525d6e853a756455aaed81e61110
bilal.zahory <bilal.zahory@consensys.net>
2023-03-21 18:12:53 -0400

    chore: add and configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link font-extrabold"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
