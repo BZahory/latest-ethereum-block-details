f1c605a59662823471da40bb2aefe16c5faed92e
bilal.zahory <bilal.zahory@consensys.net>
2023-03-23 12:06:44 -0400

    chore: add action typing

--- src/slices/ethereum-block-data/actions.ts
+++ src/slices/ethereum-block-data/actions.ts
@@ -1,4 +1,11 @@
-import { Alchemy, AssetTransfersCategory, Network, toHex, TransactionResponse } from 'alchemy-sdk';
+import {
+  Alchemy,
+  AssetTransfersCategory,
+  AssetTransfersResult,
+  Network,
+  toHex,
+  TransactionResponse,
+} from 'alchemy-sdk';
 import { ALCHEMY_API_KEY } from '../../constants';
 import { Dispatch } from 'redux';
 import {
@@ -11,8 +18,28 @@ import axios from 'axios';
 
 const alchemy = new Alchemy({ apiKey: ALCHEMY_API_KEY, network: Network.ETH_MAINNET });
 
-export const updateLatestBlock = () => async (dispatch: Dispatch<any>) => {
-  dispatch({ type: GET_LATEST_BLOCK.LOADING });
+type GetLatestBlockActionType = {
+  type: NonNullable<string>;
+  payload: { transactions: TransactionResponse[]; blockNumber: Number } | { error: unknown } | {};
+};
+
+type GetLatestEtherPriceActionType = {
+  type: NonNullable<string>;
+  payload: { price: Number } | { error: unknown } | {};
+};
+
+type GetErc20TransfersPayloadActionType = {
+  type: NonNullable<string>;
+  payload: { transfers: AssetTransfersResult[] } | { error: unknown } | {};
+};
+
+type ClearSelectedTransactionPayloadActionType = {
+  type: NonNullable<string>;
+  payload: {};
+};
+
+export const updateLatestBlock = () => async (dispatch: Dispatch<GetLatestBlockActionType>) => {
+  dispatch({ type: GET_LATEST_BLOCK.LOADING, payload: {} });
 
   try {
     const latestBlockNumber = await alchemy.core.getBlockNumber();
@@ -30,26 +57,28 @@ export const updateLatestBlock = () => async (dispatch: Dispatch<any>) => {
   }
 };
 
-export const updateLatestEtherPrice = () => async (dispatch: Dispatch<any>) => {
-  dispatch({ type: GET_LATEST_ETHER_PRICE.LOADING });
+export const updateLatestEtherPrice =
+  () => async (dispatch: Dispatch<GetLatestEtherPriceActionType>) => {
+    dispatch({ type: GET_LATEST_ETHER_PRICE.LOADING, payload: {} });
 
-  try {
-    const price: { ethereum: { usd: Number } } = await axios
-      .get(
-        `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd
+    try {
+      const price: { ethereum: { usd: Number } } = await axios
+        .get(
+          `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd
     `,
-      )
-      .then((res) => res.data);
+        )
+        .then((res) => res.data);
 
-    dispatch({ payload: { price: price.ethereum.usd }, type: GET_LATEST_ETHER_PRICE.SUCCESS });
-  } catch (e) {
-    dispatch({ payload: { error: e }, type: GET_LATEST_ETHER_PRICE.ERROR });
-  }
-};
+      dispatch({ payload: { price: price.ethereum.usd }, type: GET_LATEST_ETHER_PRICE.SUCCESS });
+    } catch (e) {
+      dispatch({ payload: { error: e }, type: GET_LATEST_ETHER_PRICE.ERROR });
+    }
+  };
 
 export const getErc20Transfers =
-  (transaction: TransactionResponse, blockNumber: number) => async (dispatch: Dispatch<any>) => {
-    dispatch({ type: GET_ERC_20_TRANSFERS.LOADING });
+  (transaction: TransactionResponse, blockNumber: number) =>
+  async (dispatch: Dispatch<GetErc20TransfersPayloadActionType>) => {
+    dispatch({ type: GET_ERC_20_TRANSFERS.LOADING, payload: {} });
 
     try {
       const fromAssetTransfers = await alchemy.core
@@ -85,8 +114,10 @@ export const getErc20Transfers =
     }
   };
 
-export const clearSelectedTransaction = () => async (dispatch: Dispatch<any>) => {
-  dispatch({
-    type: CLEAR_SELECTED_TRANSACTION,
-  });
-};
+export const clearSelectedTransaction =
+  () => async (dispatch: Dispatch<ClearSelectedTransactionPayloadActionType>) => {
+    dispatch({
+      type: CLEAR_SELECTED_TRANSACTION,
+      payload: {},
+    });
+  };
